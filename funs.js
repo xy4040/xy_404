29ec289d4e3c17f007f190942f665b3cc1fdfe15
xy4040 <xy_404@foxmail.com>
2018-04-09 17:44:54 +0800

    js通用函数

--- funs.js
+++ funs.js
@@ -6,11 +6,23 @@ author:木马牛
 
  	},
  	/*
+ 	去除字符串的空格
  	params:str(字符串)
- 	return:返回去除空格后的str
+ 	return:str
  	 */
  	str_trim : function(str){
  		if(!str)return '';
 		return str.replace(/\s/g,'');
+ 	}
+ 	/*
+ 	判断一个变量是否为数字类型
+ 	param:str(字符串)
+ 	return:blooen	true/false(是数字/不是数字)
+ 	 */
+ 	isNumber : function(str){
+ 		if(isNaN(str)){
+ 			return false;
+ 		}
+ 		return true;
  	}
  }
